280ffceb337cc5c64cd396e0bd491a02714fb15b
WhiteF326 <Lume1a326.leno@gmail.com>
2021-04-18 12:57:04 +0900

    a

---
+++ ABC/ABC045/C_Java/C.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Main {
+  static double log2(double x) {
+    return Math.log10(x) / Math.log10(2);
+  }
+
+  static int[] Dec2Bin(int x, int len) {
+    String l = Integer.toBinaryString(x);
+    int[] ret = new int[len];
+    for (int i = 0; i < l.length(); i++) {
+      ret[len - l.length() + i] = Integer.parseInt(l.substring(i, i + 1));
+    }
+    return ret;
+  }
+
+  static long sum(int[] arr) {
+    long ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans += arr[i];
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long ans = 0;
+
+    String s = sc.next();
+
+    if (s.length() == 1) {
+      System.out.println(s);
+    } else {
+      for (int i = 0; i < (1 << (s.length() - 1)); i++) {
+        int[] ret = Dec2Bin(i, s.length() - 1);
+        String t = "";
+        for (int j = 0; j < s.length() - 1; j++) {
+          t += s.substring(j, j + 1);
+          if (ret[j] == 1)
+            t += " ";
+        }
+        t += s.substring(s.length() - 1);
+        String[] cls = t.split(" ");
+        for (int j = 0; j < cls.length; j++) {
+          ans += Long.parseLong(cls[j]);
+        }
+      }
+
+      System.out.println(ans);
+    }
+
+    sc.close();
+  }
+}
---
+++ Other/jsc2021/B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fs(n) fixed << setprecision(n)
+using ll = long long;
+
+int fact(int x) { return x <= 1 ? 1 : x * fact(x - 1); }
+
+int sign(int x){
+  return (x > 0) - (x < 0);
+}
+
+bool isLeapYear(int y, int m){
+  return (y % 400 == 0 || (y % 4 == 0 && y % 100)) && m == 2;
+}
+ 
+vector<int> AllDivs(int d){
+	vector<int> resDivs(0);
+	for(int i = 1; i < (int)sqrt(d); i++){
+		if(d % i == 0){
+			resDivs.push_back(i);
+			resDivs.push_back(d / i);
+		}
+	}
+	sort(resDivs.begin(), resDivs.end());
+	return resDivs;
+}
+ 
+bool IsPrime(int num){
+	bool ans = true;
+	if(num < 2) return false;
+	else if (num == 2) return true;
+	else{
+		for(int i = 2; i <= (int)sqrt(num); i++){
+			if(num % i == 0){
+					ans = false;
+			}
+		}
+	}
+	return ans;
+}
+ 
+int NextPrime(int d){
+	int ans = d;
+	while(true){
+		ans++;
+		if(IsPrime(ans)) break;
+	}
+	return ans;
+}
+ 
+vector<int> Dec2Bin(int x, int len){
+	vector<int> res(len, 0);
+	int d = 1, pt = len - 1;
+	while(d <= x){
+		res[pt] = (x & d ? 1 : 0);
+		d *= 2; pt--;
+	}
+	return res;
+}
+ 
+int avg(int a, int b){
+  return (a + b) / 2;
+}
+
+int main(){
+	int n, m; cin >> n >> m;
+  vector<int> c(1001, 0);
+  vector<int> ans(0);
+
+  for(int i = 0; i < n; i++){
+    int n; cin >> n;
+    c[n]++;
+  }
+  for(int i = 0; i < m; i++){
+    int n; cin >> n;
+    c[n]++;
+  }
+  for(int i = 1; i <= 1000; i++){
+    if(c[i] == 1) ans.push_back(i);
+  }
+  if(ans.size() > 0){
+    for(int i = 0; i < ans.size() - 1; i++){
+      cout << ans[i] << " ";
+    }
+    cout << ans[ans.size() - 1];
+  }
+  cout << endl;
+  
+}
