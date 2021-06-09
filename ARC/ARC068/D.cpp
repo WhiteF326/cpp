e51c507e824c30523e1889c22e59821790b8f045
WhiteF326 <Lume1a326.leno@gmail.com>
2021-06-09 10:18:35 +0900

    s

---
+++ ARC/ARC068/D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#define fs(n) fixed << setprecision(n)
+#define mp(i, j) make_pair(i, j);
+using ll = long long;
+using ld = long double;
+
+const ll modv = 1000000007;
+
+int main(){
+  
+}
--- Other/code-thanks-festival-2017-open/C.cpp
+++ Other/code-thanks-festival-2017-open/C.cpp
@@ -12,5 +12,20 @@ const ll modv = 1000000007;
 
 int main(){
   int n, k; cin >> n >> k;
-  
+  priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q;
+  for(int i = 0; i < n; i++){
+    ll a, b; cin >> a >> b;
+    q.push(make_pair(a, b));
+  }
+  ll ans = 0;
+  for(int i = 0; i < k; i++){
+    auto x = q.top();
+    q.pop();
+
+    ans += x.first;
+    
+    x.first += x.second;
+    q.push(x);
+  }
+  cout << ans << endl;
 }
