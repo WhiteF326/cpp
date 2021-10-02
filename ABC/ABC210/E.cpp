d89afed927a080b8a7d1b11b094fa4bfa606538d
WhiteF326 <Lume1a326.leno@gmail.com>
2021-10-02 17:38:46 +0900

    abc210e

--- ABC/ABC210/E.cpp
+++ ABC/ABC210/E.cpp
@@ -2,12 +2,40 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-
-#define fs(n) fixed << setprecision(n);
-#define mp(a, b) make_pair(a, b);
+ 
+#define fs(n) fixed << setprecision(n)
+#define mp(a, b) make_pair(a, b)
+#define all(x) x.begin(), x.end()
+#define const constexpr
+#define pdesc(t) t, vector<t>, greater<t>
 using ll = long long;
 using ld = long double;
+#define query(t) for(int _ = 0; _ < t; _++)
+#define aryin(a, n) for(int i = 0; i < n; i++) cin >> a[i];
 
 int main(){
-  
+  int n; cin >> n;
+  int m; cin >> m;
+  vector<pair<int, ll>> ac(m);
+  map<int, ll> cl;
+  ll mc = LLONG_MAX;
+  for(int i = 0; i < n; i++){
+    int a; ll c;
+    cin >> a >> c;
+    ac[i].first = a, ac[i].second = c;
+    mc = min(mc, c);
+    if(!cl[a]){
+      cl[a] = c;
+    }else{
+      cl[a] = min(cl[a], c);
+    }
+  }
+
+  multiset<ll> mxl;
+  for(auto p : cl) mxl.insert(p.second);
+
+  ll ans = LLONG_MAX;
+  for(int i = 0; i < m; i++){
+    //
+  }
 }
--- Main.java
+++ Main.java
@@ -1,50 +1,51 @@
 import java.util.*;
 
-class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    // 入力
-    int n = sc.nextInt();
-    int m = sc.nextInt();
-
-    boolean[] visited = new boolean[n]; // 既に訪れたか？
-    ArrayList<ArrayList<Integer>> roots = new ArrayList<ArrayList<Integer>>(); // グラフ本体
-
-    // 初期化
-    int[] dist = new int[n]; // 開始点からみた最短距離
-    for (int i = 0; i < n; i++) {
-      roots.add(new ArrayList<Integer>());
-      visited[i] = false;
-    }
-    visited[0] = true; // 開始点は既に「訪れた」
-    dist[0] = 0; // 開始点の距離は0
-
-    // 入力
-    for (int i = 0; i < m; i++) {
-      int a = sc.nextInt(), b = sc.nextInt();
-      a--;
-      b--;
-      roots.get(a).add(b);
-    }
-
-    // ここからBFS本体
-    Deque<Integer> stack = new ArrayDeque<>();
-    stack.push(0); // 開始点は最初にpushする
-    while (stack.size() > 0) {
-      int point = stack.pop();
-      for (int i = 0; i < roots.get(point).size(); i++) {
-        if (visited[roots.get(point).get(i)] == false) {
-          visited[roots.get(point).get(i)] = true;          // 訪れた
-          dist[roots.get(point).get(i)] = dist[point] + 1;  // 最短距離を入れる
-          stack.push(roots.get(point).get(i));              // 次の探索位置をpush
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int m = n * 2;
+        
+        if(n > 18){
+            System.out.println(0);
+            sc.close();
+            return;
         }
-      }
-    }
+        
+        int ans = 0;
+        for(int i = 0; i < (1 << m); i++){
+            TreeSet<Integer> a = new TreeSet<>();
+            for(int j = 0; j < m; j++) a.add(j);
+            TreeSet<Integer> b = new TreeSet<>();
+            
+            for(int j = 0; j < m; j++){
+                if((i & (1 << j)) != 0){
+                    a.remove(j);
+                    b.add(j);
+                }
+            }
 
-    // 結果
-    System.out.println(dist[n - 1] == 2 ? "POSSIBLE" : "IMPOSSIBLE");
-    sc.close();
-  }
+            // System.out.println(b.size());
+            
+            if(a.size() != b.size()) continue;
+            
+            int[] ar = a.stream().mapToInt(Number::intValue).toArray();
+            int[] br = b.stream().mapToInt(Number::intValue).toArray();
+            
+            int diffsum = 0;
+            for(int j = 0; j < n; j++){
+                diffsum += (int)Math.abs(ar[j] - br[j]);
+            }
+
+            if(diffsum <= k) ans++;
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+    }
 }
