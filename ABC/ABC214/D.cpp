2ca353c5b1c6c87daa0f6f29f38297a82f57b4a9
WhiteF326 <Lume1a326.leno@gmail.com>
2021-10-07 12:54:43 +0900

    push

--- ABC/ABC214/D.cpp
+++ ABC/ABC214/D.cpp
@@ -2,15 +2,18 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-
+ 
 #define fs(n) fixed << setprecision(n)
 #define mp(a, b) make_pair(a, b)
 #define all(x) x.begin(), x.end()
-// #define const constexpr
+#define const constexpr
 #define pdesc(t) t, vector<t>, greater<t>
 using ll = long long;
 using ld = long double;
+#define query(t) for(int _ = 0; _ < t; _++)
+#define aryin(a, n) vector<int> a(n); for(int i = 0; i < n; i++) cin >> a[i];
 
 int main(){
+  int n; cin >> n;
   
 }
