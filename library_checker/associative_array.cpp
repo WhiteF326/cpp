4828bc1bc17cfa33ac9a3420de89812641346882
WhiteF326 <Lume1a326.leno@gmail.com>
2022-02-26 19:03:38 +0900

    a

--- library_checker/associative_array.cpp
+++ library_checker/associative_array.cpp
@@ -7,11 +7,11 @@ using namespace std;
 using namespace atcoder;
 
 #define fs(n) fixed << setprecision(n)
-#define mp(a, b) make_pair(a, b)
 #define all(x) x.begin(), x.end()
+#define pdesc(t) t, vector<t>, greater<t>
 using ll = long long;
 using ld = long double;
-#define query(t) while(t--)
+#define query(t) for(int _ = 0; _ < t; _++)
 #define aryin(a, n) for(int i = 0; i < n; i++) cin >> a[i];
 #define chmin(a, b) a = min(a, b)
 #define chmax(a, b) a = max(a, b)
@@ -21,18 +21,16 @@ int main(){
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
   
-  int n, x; cin >> n >> x;
-  vector<vector<bool>> dp(n + 1, vector<bool>(x + 1, 0));
-  dp[0][0] = 1;
-
-  for(int i = 0; i < n; i++){
-    int a, b; cin >> a >> b;
-    for(int j = 0; j <= x; j++){
-      if(!dp[i][j]) continue;
-      if(j + a <= x) dp[i + 1][j + a] = 1;
-      if(j + b <= x) dp[i + 1][j + b] = 1;
+  unordered_map<ll, ll> m;
+  int q; cin >> q;
+  query(q){
+    int t; cin >> t;
+    if(t == 0){
+      ll k, v; cin >> k >> v;
+      m[k] = v;
+    }else{
+      ll k; cin >> k;
+      cout << m[k] << endl;
     }
   }
-
-  cout << (dp[n][x] ? "Yes" : "No") << endl;
 }
